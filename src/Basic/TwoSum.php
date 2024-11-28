91419108a09d63958033dca3dcb9aca11f72f970
Felipe Mendes <felipemendesdev@gmail.com>
2024-11-28 20:52:56 -0300

    feat: init project and solve the twoSum algorithm

---
+++ src/Basic/TwoSum.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Algo\Basic;
+
+class TwoSum
+{
+    public function handler(array $nums, int $target): array
+    {
+        return $this->solutionOne($nums, $target);
+    }
+
+    private function solutionOne(array $nums, int $target): array //n log n
+    {
+        sort($nums);
+        $left = 0;
+        $right = count($nums) - 1;
+        while ($left < $right) {
+            $sum = $left + $right;
+            if ($sum === $target){
+                return [$nums[$left], $nums[$right]];
+            }
+            if ($sum < $target) {
+                $left++;
+            }
+            if ($sum > $target) {
+                $right--;
+            }
+        }
+        return [];
+    }
+}
---
+++ tests/Unit/TwoSumTest.php
@@ -0,0 +1,10 @@
+<?php
+use Algo\Basic\TwoSum;
+
+test('two sum algorithm', function () {
+    $target = 9 ;
+    $nums = [4, 1, 2, -2, 11, 15, 1, -1, -6, -4];
+    $result = new TwoSum()->handler($nums, $target);
+    $sum = array_sum($result);
+    expect($sum)->toBe($target);
+});
